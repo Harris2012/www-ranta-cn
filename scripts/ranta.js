6b19ee3919131f2b77029c87fc8ab989b562736b
HarrisZhang <hc_zhang@Ctrip.com>
2018-01-08 11:54:03 +0800

    update bundle

--- scripts/ranta.js
+++ scripts/ranta.js
@@ -25,3 +25,24 @@ function FavoriteService($resource, $q) {
     }
 
 }
+var route = function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.when('', '/welcome').when('/', '/welcome');
+
+    $stateProvider.state('app', {
+        url: '/'
+    });
+
+    $stateProvider.state('app.welcome', {
+        url: 'welcome',
+        templateUrl: 'scripts/views/view_welcome.html?v=' + window.version,
+        controller: WelcomeController
+    });
+}
+var app = angular.module('app', ['ngResource', 'ui.router']);
+
+app.config(route);
+
+app.service('FavoriteService', ['$resource', '$q', FavoriteService]);
+
+app.controller(WelcomeController);
